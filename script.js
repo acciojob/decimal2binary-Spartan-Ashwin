7e67161fc80337b3a904531558a271d7828b8ad8
Spartan-Ashwin <123223152+Spartan-Ashwin@users.noreply.github.com>
2023-10-23 21:20:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function decimalToBinary(num) {
   //Write you code here
 	let ans=0;
 	while(num!=0){
-		ans=ans*10+num%2;
+		ans+=num%2+"";
 		num=num/2;
 	}
-	return ans;
+	return ans.reverse();
   
 }
 
